4576036a6198a4616283efd7d0c63bb2859c706e
kaitabuchi <kaidanieltabuchi@gmail.com>
2024-05-21 08:03:19 -0500

    add level 1

---
+++ levels/setpos.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+@export var follow : Camera2D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = follow.get_global_transform().get_origin()
